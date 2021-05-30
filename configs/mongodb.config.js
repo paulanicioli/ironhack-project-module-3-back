c65367651fbbba167f75c7168afb7af0c3b4752c
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-05-30 13:29:01 -0300

    set modern options to useCreateIndex and useFindAndModify

--- configs/mongodb.config.js
+++ configs/mongodb.config.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => console.log('Sucessfully connected to the Database'))
   .catch((error) => console.log(error));
