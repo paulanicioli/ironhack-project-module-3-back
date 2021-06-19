b897825a7acae209c83dc19219925f51744534c0
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-06-19 16:58:22 -0300

    add mapsAPI handler with geocoding functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,11 +10,16 @@ const businessRoutes = require('./business/business.routes');
 const businessCategoriesRoutes = require('./businessCategories/businessCategories.routes');
 const productsRoutes = require('./products/products.routes');
 
+const maps = require('../controller/mapsApi.controller')
+
 router.use('/auth', authRoutes);
 
 router.use('/categories', businessCategoriesRoutes);
 router.use('/businesses', businessRoutes);
 router.use('/products', productsRoutes);
+
+//DELETE ON PRODUCTION!!!
+router.get('/geocode', maps.getGeocode)
 
 // Middleware to protect loggedin-only routes
 router.use(protectedRoutesMiddleware.protect(2));
